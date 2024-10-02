fd564d9263a728f9dcfdb7756a2fba6dbae70b57
Ana Luiz <acluiz.study@gmail.com>
2024-10-01 21:24:55 -0300

    feat: add ws connection

--- ui/src/components/messages.tsx
+++ ui/src/components/messages.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 
-import { getRoomMessages } from "../services/get-room-messages";
+import {
+  GetRoomMessageResponse,
+  getRoomMessages,
+} from "../services/get-room-messages";
 
 import { Message } from "./message";
 
+type MessageKind =
+  | "message_created"
+  | "message_answered"
+  | "message_reaction_increased"
+  | "message_reaction_decreased";
+
+interface WSUpdatedResponse {
+  kind: MessageKind;
+  value: any;
+}
+
 export const Messages = () => {
   const { roomID } = useParams();
+  const queryClient = useQueryClient();
 
   if (!roomID) {
     throw new Error("Messages components must be used within room page");
@@ -16,6 +32,88 @@ export const Messages = () => {
     queryKey: ["messages", roomID],
     queryFn: () => getRoomMessages({ roomID }),
   });
+
+  const onMessageCreated = (data: WSUpdatedResponse) => {
+    queryClient.setQueryData<GetRoomMessageResponse[]>(
+      ["messages", roomID],
+      (state) => [
+        ...(state ?? []),
+        {
+          id: data.value.id,
+          message: data.value.message,
+          reaction_count: 0,
+          answered: false,
+        },
+      ]
+    );
+  };
+
+  const onMessageAnswered = (data: WSUpdatedResponse) => {
+    queryClient.setQueryData<GetRoomMessageResponse[]>(
+      ["messages", roomID],
+      (state) => {
+        if (!state) {
+          return undefined;
+        }
+
+        return state.map((message) => {
+          if (message.id === data.value.id) {
+            return { ...message, answered: true };
+          }
+
+          return message;
+        });
+      }
+    );
+  };
+
+  const onMessageReacted = (data: WSUpdatedResponse) => {
+    queryClient.setQueryData<GetRoomMessageResponse[]>(
+      ["messages", roomID],
+      (state) => {
+        if (!state) {
+          return undefined;
+        }
+
+        return state.map((message) => {
+          if (message.id === data.value.id) {
+            return { ...message, reaction_count: data.value.count };
+          }
+
+          return message;
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:8080/subscribe/${roomID}`);
+
+    ws.onopen = () => {
+      console.log("Websocket connected");
+    };
+
+    ws.onclose = () => {
+      console.log("Websocket connection closed");
+    };
+
+    ws.onmessage = (e) => {
+      const data: WSUpdatedResponse = JSON.parse(e.data);
+
+      const actions = {
+        message_created: onMessageCreated(data),
+        message_answered: onMessageAnswered(data),
+        message_reaction_increased: onMessageReacted(data),
+        message_reaction_decreased: onMessageReacted(data),
+      };
+
+      const actionToTrigger = () => actions[data.kind];
+
+      actionToTrigger();
+    };
+
+    return () => ws.close();
+  }, [roomID, queryClient]);
 
   return (
     <ol className="list-decimal list-outside px-3 space-y-8">
--- ui/src/services/get-room-messages.ts
+++ ui/src/services/get-room-messages.ts
@@ -2,19 +2,17 @@ interface GetRoomMessagesRequest {
   roomID: string;
 }
 
-interface GetRoomMessageResponse {
+export interface GetRoomMessageResponse {
   id: string;
-  room_id: string;
   message: string;
   reaction_count: number;
   answered: boolean;
 }
 
 // Temporary mock - waiting for method to be created in api
-const MOCKED_RESPONSE = [
+const MOCKED_RESPONSE: GetRoomMessageResponse[] = [
   {
     id: "0",
-    room_id: "0",
     message:
       "O que é GoLang e quais são suas principais vantagens em comparação com outras linguagens de programação como Python, Java ou C++?",
     reaction_count: 182,
@@ -22,7 +20,6 @@ const MOCKED_RESPONSE = [
   },
   {
     id: "1",
-    room_id: "1",
     message:
       "Como funcionam as goroutines em GoLang e por que elas são importantes para a concorrência e paralelismo?",
     reaction_count: 173,
@@ -30,14 +27,16 @@ const MOCKED_RESPONSE = [
   },
 ];
 
-export const getRoomMessages = async ({ roomID }: GetRoomMessagesRequest) => {
+export const getRoomMessages = async ({
+  roomID,
+}: GetRoomMessagesRequest): Promise<GetRoomMessageResponse[]> => {
   const response = await fetch(
     `${import.meta.env.VITE_APP_API_URL}/rooms/${roomID}/messages`,
     { method: "GET" }
   );
 
   try {
-    const data: Array<GetRoomMessageResponse> = await response.json();
+    const data: GetRoomMessageResponse[] = await response.json();
     return data;
   } catch {
     return MOCKED_RESPONSE;
